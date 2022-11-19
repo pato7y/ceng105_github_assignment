6483b2ccda46eaa29593956740e5d61e3557a311
Your Name <serhatyildiz721@gmail.com>
2022-11-19 18:28:03 +0300

    Update assignment.c
    adding description of adding x2 and y2 values

--- assignment.c
+++ assignment.c
@@ -12,10 +12,12 @@ int main(){
     printf("Input first points y1 coordinate: ");
     scanf("%f", &y1);
     // to check the value of y1
-    printf("Input second points x coordinate: ");
+    printf("Input second points x2 coordinate: ");
     scanf("%f", &x2);
-    printf("Input second points y coordinate: ");
+    // to check the value of x2
+    printf("Input second points y2 coordinate: ");
     scanf("%f", &y2);
+    // to check the value of y2
     gdistance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
     printf("Distance between the said points: %.4f", sqrt(gdistance));
     printf("\n");
