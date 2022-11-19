9bc8f5788f759ccd9b93b3207991e2bafae1a992
Your Name <serhatyildiz721@gmail.com>
2022-11-19 18:46:41 +0300

    serhat yıldız the first x and y values were specified //first of commit
    the first x and y values were specified //first of commit

--- assignment.c
+++ assignment.c
@@ -6,20 +6,18 @@ int main(){
     //the program that finds the distance between two points
 
     float x1, y1, x2, y2, gdistance;
+    printf("Input first points x coordinate: ");
     printf("Input first points x1 coordinate: ");
     scanf("%f", &x1);
+    printf("Input first points y coordinate: ");
     //to check the value of x1
     printf("Input first points y1 coordinate: ");
     scanf("%f", &y1);
     // to check the value of y1
     printf("Input second points x coordinate: ");
-    printf("Input second points x2 coordinate: ");
     scanf("%f", &x2);
     printf("Input second points y coordinate: ");
-    // to check the value of x2
-    printf("Input second points y2 coordinate: ");
     scanf("%f", &y2);
-    // to check the value of y2
     gdistance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
     printf("Distance between the said points: %.4f", sqrt(gdistance));
     printf("\n");
